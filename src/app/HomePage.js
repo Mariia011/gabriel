1816f0d4c1a7da75cbf83ecd034b0a9e761b32bf
Mariia Khachaturova <maria.olegovnakh@gmail.com>
2024-12-17 18:39:11 +0400

    test1111

---
+++ src/app/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>GABRIEL RECORDS</h1>
+      <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown.... It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  </p>  
+    
+    </div>
+  )
+}
+
+export default HomePage
